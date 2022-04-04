a38f89346ba8e8af1cbd665cbaf3a2296aec9233
Bhupinderjit Singh <pindio58@gmail.com>
2022-04-05 00:41:02 +0530

    added README file and changed the code format

---
+++ README.md
@@ -0,0 +1,15 @@
+# Overview
+
+Recently I was working on a PL/SQL project and I had so many unsaved tabs open on SQLDeveloper. Before I could save those queries, It crashed and got closed.
+It led me to think and prepare a project to get those queries and save it.
+
+## How this works
+
+Since I am using WSL2 on Windows 10, by default all Oracle SQL history(queries) are saved on location `C:\Users\pc\AppData\Roaming\SQL Developer\SqlHistory`. So this will be the input to the function.
+
+* Create a `.sql` file in current directory whose by default name is `allSqls.sql`, which can be changed as per user's wish.
+* Add the current time at top of the file.
+* Traverse through all `xml` files from input directory and extract the text from `SQL` tag.
+* Add that text to the `sql` file.
+
+**P.S.** Pressing F8 will also give us similar SQL history
--- extractSqlsFromSQLhistoryXMLfiles.py
+++ extractSqlsFromSQLhistoryXMLfiles.py
@@ -3,7 +3,6 @@ from pathlib import Path
 from xml.dom import minidom
 import datetime
 
-
 # Input directory
 # I am using WSL2 on windows OS. Please change it accordingly
 inputDir = Path.home().parent.parent.joinpath(
@@ -16,10 +15,17 @@ outputfile = str(Path.cwd().joinpath('allSqls.sql'))
 # time for extraction
 timeNow = datetime.datetime.today().strftime('%d-%b-%Y-%H:%M:%S')
 
-# Traverse all xml files and extract text SQL tag
-with open(outputfile,'w') as f:
-    f.write(f'--====================================\n-- Extracted at {timeNow} =\n--====================================\n\n')        
-    for num, file in enumerate(list(inputDir.glob('*xml'))[::-1]):
-        doc = minidom.parse(str(file))
-        f.write(f'\n\n--=================================  {num}  ================================================\n\n')
-        f.write(doc.getElementsByTagName("sql")[0].firstChild.data)             
+# Define function and traverse all xml files and extract text SQL tag
+def extract(inputDir, outputfile):
+    with open(outputfile, 'w') as f:
+        f.write(
+            f'--====================================\n-- Extracted at {timeNow} =\n--====================================\n\n')
+        for num, file in enumerate(list(inputDir.glob('*xml'))[::-1]):
+            doc = minidom.parse(str(file))
+            f.write(
+                f'\n\n--=================================  {num}  ================================================\n\n')
+            f.write(doc.getElementsByTagName("sql")[0].firstChild.data)
+
+
+if __name__ == '__main__':
+    extract(inputDir, outputfile)
